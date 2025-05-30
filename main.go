24b2106a3a3dcd3b872687740049348134cee2e2
Dimash <dinagayda@gmail.com>
2025-05-30 18:39:58 +0500

    var

--- main.go
+++ main.go
@@ -3,7 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
+
+	//score := 0
+	//text := "Gave over"
+	//
+	//score = 50
+	//text = "Hello world"
+	//
+	//score = 60 + 70
+	//
+	//score = score + 50
+	//fmt.Println(score)
+	//fmt.Println(text)
+
+	score := 0
+	text := "Game ready!"
+	//text2 := "Game over!"
+
+	fmt.Println(text)
+	fmt.Println("Score:", score)
+
+	fmt.Println("Вы пролетели трубу")
+	score = score + 1
+	fmt.Println("Score:", score)
+
+	fmt.Println("Вы пролетели трубу")
+	score = score + 1
+	fmt.Println("Score:", score)
+
+	fmt.Println("Вы пролетели трубу")
+	score = score + 1
+	fmt.Println("Score:", score)
+
+	fmt.Println("Вы пролетели трубу")
+	score = score + 1
+	fmt.Println("Score:", score)
+
+	fmt.Println("Вы пролетели трубу")
+	score = score + 1
+	fmt.Println("Score:", score)
+
 }
