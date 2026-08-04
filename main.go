5b932ce48eec2964d0ccda2e4fc148d6e1555630
lvlupcode10 <dinagayda@gmail.com>
2026-08-05 03:33:39 +0500

    3:42:15 continue

--- main.go
+++ main.go
@@ -3,11 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	score := 5
+	// score := 5
 
-	if score > 10 {
-		fmt.Println("GO")
+	// if score > 10 {
+	// 	fmt.Println("GO")
+	// } else {
+	// 	fmt.Println("STOP")
+	// }
+
+	// score := 11
+	// if score > 10 {
+	// 	fmt.Println("Ты красавчик")
+	// } else if score >= 15 {
+	// 	fmt.Println("Мега красавчик")
+	// } else {
+	// 	fmt.Println("Надо стараться")
+	// }
+
+	score := 56
+	if score == 12 {
+		fmt.Println("Дюжина")
+	} else if score == 21 {
+		fmt.Println("Очко!")
+
+	} else if score == 50 {
+		fmt.Println("Полтинник")
 	} else {
-		fmt.Println("STOP")
+		fmt.Println("Ты не попал не в какую категорию!")
 	}
+
+	// hasSub := false
+	// if hasSub {
+	// 	fmt.Println("Ты подписан")
+	// } else {
+	// 	fmt.Println("Не подписан")
+	// }
+
 }
