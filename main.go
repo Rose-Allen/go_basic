4fd1b7c1f291f3ec0a2cead82bebde493dae5fbf
lvlupcode10 <dinagayda@gmail.com>
2026-08-06 04:34:49 +0500

    full conditions

--- main.go
+++ main.go
@@ -20,17 +20,17 @@ func main() {
 	// 	fmt.Println("Надо стараться")
 	// }
 
-	score := 56
-	if score == 12 {
-		fmt.Println("Дюжина")
-	} else if score == 21 {
-		fmt.Println("Очко!")
+	// score := 56
+	// if score == 12 {
+	// 	fmt.Println("Дюжина")
+	// } else if score == 21 {
+	// 	fmt.Println("Очко!")
 
-	} else if score == 50 {
-		fmt.Println("Полтинник")
-	} else {
-		fmt.Println("Ты не попал не в какую категорию!")
-	}
+	// } else if score == 50 {
+	// 	fmt.Println("Полтинник")
+	// } else {
+	// 	fmt.Println("Ты не попал не в какую категорию!")
+	// }
 
 	// hasSub := false
 	// if hasSub {
@@ -38,5 +38,36 @@ func main() {
 	// } else {
 	// 	fmt.Println("Не подписан")
 	// }
+
+	// score := 20
+	// if score < 6 || score > 16 {
+	// 	fmt.Println("Попал в петушению зону")
+	// } else {
+	// 	fmt.Print("Не попал в зону")
+	// }
+
+	// score := 9
+	// if score > 8 && score < 15 {
+	// 	fmt.Println("Попал в яблочко")
+	// } else {
+	// 	fmt.Print("Не попал в зону")
+	// }
+
+	// score := 10
+	// if score >= 10 {
+	// 	fmt.Println("GOOOOOOOOOOOOOOD!")
+	// } else {
+	// 	fmt.Print("NOT GOOD!")
+	// }
+	score := 1
+	if score != 7 {
+
+		fmt.Println("Победил")
+	} else {
+		fmt.Println("Проиграл")
+	}
+
+	check := 7 != 7
+	fmt.Print(check)
 
 }
