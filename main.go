d59846b7191471ca0fa0eb81b70a4c30d29264e2
lvlupcode10 <dinagayda@gmail.com>
2026-08-31 04:26:17 +0500

    loops

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
+	// fmt.Print("Hello World")
+	// fmt.Print("Hello World")
+	// fmt.Print("Hello World")'
+
+	for i := 1; i <= 5; i++ {
+		fmt.Print("Hello World")
+	}
 }
