6da0bf289e84fb7e0eaa7dad24ebc779c38b2585
Dimash <d.esengeldiev@mail.ru>
2026-08-31 23:57:14 +0500

    loops 5:13:03

--- main.go
+++ main.go
@@ -1,13 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// fmt.Print("Hello World")
 	// fmt.Print("Hello World")
 	// fmt.Print("Hello World")'
 
-	for i := 1; i <= 5; i++ {
-		fmt.Print("Hello World")
-	}
+	// for i := 1; i <= 5; i++ {
+	// 	fmt.Print("Hello World")
+	// }
+
+
+	// fmt.Println("До")
+	// number:= 999;
+	// fmt.Println(number)
+
+	// for i:=1; i<= 5; i++{
+	// 	score:= 5;
+	// 	score = score +3;
+	// 	fmt.Println("Hello world", i)
+	// 	fmt.Println("score = ", score)
+	// 	fmt.Println("number = ", number)
+	// 	number++;
+	// }
+	// fmt.Println("После number",number);
+
+
+	
+	score:= 0
+	fmt.Println("Get Ready")
+	fmt.Println("Счет: ", score)
+	fmt.Println("")
+	fmt.Println("Вы подлетаете к трубе! 1")
+	fmt.Println("🐥 🟩 🟩 ")
+	fmt.Println("")
+
+	fmt.Println("Вы подлетаете через трубу! 1")
+	fmt.Println(" 🟩 🐥 🟩 ")
+	fmt.Println("")
+
+	fmt.Println("Вы пролетели через трубу! 1")
+	fmt.Println(" 🟩 🟩 🐥 ")
+	fmt.Println("")
+
+
+	score++
+
+	fmt.Println("Счет: ", score)
+
 }
