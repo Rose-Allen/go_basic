beb1a48b1079dc4817b7ee59d5abfc0b544dd3e7
lvlupcode10 <dinagayda@gmail.com>
2026-09-01 03:08:31 +0500

    loops

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -12,7 +14,6 @@ func main() {
 	// for i := 1; i <= 5; i++ {
 	// 	fmt.Print("Hello World")
 	// }
-
 
 	// fmt.Println("До")
 	// number:= 999;
@@ -27,28 +28,88 @@ func main() {
 	// 	number++;
 	// }
 	// fmt.Println("После number",number);
+	//🟥
 
+	// score := 0
+	// fmt.Println("Get Ready")
+	// fmt.Println("Счет: ", score)
+	// fmt.Println("")
+	// for i := 1; i <= 5; i++ {
 
-	
-	score:= 0
+	// 	fmt.Println("==========================================")
+
+	// 	fmt.Println("Вы подлетаете к трубе! ", i)
+	// 	fmt.Println("🐥 🟩 🟩 ")
+	// 	fmt.Println("")
+
+	// 	fmt.Println("Вы подлетаете через трубу! ", i)
+	// 	fmt.Println(" 🟩 🐥 🟩 ")
+	// 	fmt.Println("")
+
+	// 	fmt.Println("Вы пролетели через трубу! ", i)
+	// 	fmt.Println(" 🟩 🟩 🐥 ")
+	// 	fmt.Println("")
+
+	// 	score++
+
+	// 	fmt.Println("Счет: ", score)
+
+	// }
+
+	// fmt.Println("Начинаю уровень генерации трубы!")
+
+	// for i := 1; i <= 10; i++ {
+	// 	fmt.Println("Труба номер", i)
+	// 	fmt.Println("=========================")
+	// 	if i%2 == 0 {
+	// 		fmt.Println("🟥 🟥")
+	// 	} else {
+	// 		fmt.Println("🟩 🟩")
+
+	// 	}
+
+	// 	fmt.Println("=========================")
+	// 	fmt.Println("")
+	// }
+
+	// fmt.Println("Закончил уровень генерации трубы")
+
+	// fmt.Println("Hello world")
+	// fmt.Println("Рандомная цифра", rand.Intn(10))
+
+	score := 0
 	fmt.Println("Get Ready")
 	fmt.Println("Счет: ", score)
 	fmt.Println("")
-	fmt.Println("Вы подлетаете к трубе! 1")
-	fmt.Println("🐥 🟩 🟩 ")
-	fmt.Println("")
+	for {
 
-	fmt.Println("Вы подлетаете через трубу! 1")
-	fmt.Println(" 🟩 🐥 🟩 ")
-	fmt.Println("")
+		fmt.Println("==========================================")
 
-	fmt.Println("Вы пролетели через трубу! 1")
-	fmt.Println(" 🟩 🟩 🐥 ")
-	fmt.Println("")
+		fmt.Println("Вы подлетаете к трубе! ")
+		fmt.Println("🐥 🟩 🟩 ")
+		fmt.Println("")
 
+		fmt.Println("Вы подлетаете через трубу! ")
+		fmt.Println(" 🟩 🐥 🟩 ")
+		fmt.Println("")
 
-	score++
+		if rand.Intn(4) == 1 {
+			fmt.Println("Я врезался в трубу")
 
-	fmt.Println("Счет: ", score)
+			break
+		}
+
+		fmt.Println("Вы пролетели через трубу! ")
+		fmt.Println(" 🟩 🟩 🐥 ")
+		fmt.Println("")
+
+		score++
+		fmt.Println("Счет: ", score)
+		fmt.Println("==========================================")
+		fmt.Println("")
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	fmt.Println("GAME OVER", score)
 
 }
