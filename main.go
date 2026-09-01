3905951434062c308eda8f7e7f1a0a4a85749e4f
lvlupcode10 <dinagayda@gmail.com>
2026-09-02 00:16:42 +0500

    дефер работает по принципу стэка первый зашел последний вышел

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
+	fmt.Println("Я main и я начался")
+	defer func() {
+		fmt.Println("Я main и я закончился")
+	}()
+	hello()
+
+}
+
+func hello() {
+	fmt.Println("Hello world1")
+	fmt.Println("Hello world2")
+	fmt.Println("Hello world3")
+
 }
