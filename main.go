c48a5fddfb5736d656f90e781029d902ad7c0069
lvlupcode10 <dinagayda@gmail.com>
2026-09-14 19:39:57 +0500

    prac

--- main.go
+++ main.go
@@ -19,6 +19,32 @@ func main() {
 
 	summa := sum(2, 2)
 	fmt.Println(summa)
+
+	res := multiply(5, 4)
+	fmt.Println(res)
+
+	fmt.Print(isEven(10))
+	fmt.Print(isEven(7))
+
+	fmt.Println(max(10, 20))
+	fmt.Println(max(50, 7))
+
+	fmt.Println(canLogin(25, false)) // true
+	fmt.Println(canLogin(16, false)) // false
+	fmt.Println(canLogin(25, true))  // false
+
+	name, age := getPerson()
+	fmt.Println(name)
+	fmt.Println(age)
+
+	sum, multiply := calculate(5, 3)
+
+	fmt.Println(sum)      // 8
+	fmt.Println(multiply) // 15
+
+	_, age2 := getPerson()
+	fmt.Println(age2)
+
 }
 
 func square(x int) {
@@ -44,4 +70,41 @@ func sum(a int, b int) int {
 	fmt.Println("b: ", b)
 	c := a + b
 	return c
+}
+
+func multiply(a int, b int) int {
+	res := a * b
+	return res
+}
+
+func isEven(num int) bool {
+	if num%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func canLogin(age int, isBlocked bool) bool {
+	if age >= 18 && !isBlocked {
+		return true
+	} else {
+		return false
+	}
+}
+
+func getPerson() (string, int) {
+	return "Dimash", 25
+}
+
+func calculate(a int, b int) (int, int) {
+	return a + b, a * b
 }
