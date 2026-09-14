d36d2809cf1149cc4abc21d250971fff6ca7e687
lvlupcode10 <dinagayda@gmail.com>
2026-09-15 02:07:46 +0500

    prac

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name        string
+	Age         int
+	PhoneNumber string
+	isClose     bool
+	Rating      float64
+}
+
 func main() {
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
-	fmt.Print("Hello World")
+	user := User{
+		Name:        "Dimash",
+		Age:         25,
+		PhoneNumber: "7078886644",
+		isClose:     false,
+		Rating:      5.5,
+	}
+	fmt.Print(user)
 }
